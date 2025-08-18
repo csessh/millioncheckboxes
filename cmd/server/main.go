3a2f4818d3c477ae065e2980e7e4dae492b12731
Thang Do <csessh@hey.com>
2025-08-18 15:24:09 +0700

    Simple ws implementation (without TLS)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,18 +4,49 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, Go backend 🚀")
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Upgrade error:", err)
+		return
+	}
+	defer conn.Close()
+
+	log.Println("Client connected")
+
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Read error:", err)
+			break
+		}
+
+		log.Printf("Received: %s\n", msg)
+
+		err = conn.WriteMessage(websocket.TextMessage, []byte("Echo: "+string(msg)))
+		if err != nil {
+			log.Println("Write error:", err)
+			break
+		}
+	}
 }
 
 func main() {
-	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/ws", wsHandler)
 
 	addr := ":8080"
-	log.Printf("Server starting at %s\n", addr)
+	fmt.Println("WebSocket server listening on", addr)
 	if err := http.ListenAndServe(addr, nil); err != nil {
-		log.Fatalf("could not start server: %v\n", err)
+		log.Fatal("Server error:", err)
 	}
 }
