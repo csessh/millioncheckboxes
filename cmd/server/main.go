7035fd9eb0e11e7d2cb0c06b9c86083492fde283
Thang Do <csessh@hey.com>
2025-08-18 16:42:42 +0700

    Accepting json data from FE client

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -17,6 +18,12 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
+type Message struct {
+	Cmd   string `json:"cmd"`
+	Key   string `json:"key,omitempty"`
+	Value string `json:"value,omitempty"`
+}
+
 func wsHandler(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -24,43 +31,46 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer conn.Close()
-
 	log.Println("Client connected")
 
+	for i := 0; i < 100; i++ {
+		key := fmt.Sprintf("cb-%d", i)
+		val, _ := redis.Get(key)
+		if val == "true" {
+			msg := Message{Cmd: "SET", Key: key, Value: "true"}
+			data, _ := json.Marshal(msg)
+			conn.WriteMessage(websocket.TextMessage, data)
+		}
+	}
+
 	for {
-		_, msg, err := conn.ReadMessage()
+		_, msgBytes, err := conn.ReadMessage()
 		if err != nil {
 			log.Println("Read error:", err)
 			break
 		}
-		text := string(msg)
-		log.Printf("Received: %s\n", text)
 
-		if err := redis.Set("last_message", text, 10*time.Minute); err != nil {
-			log.Println("Redis SET error:", err)
+		var msg Message
+		if err := json.Unmarshal(msgBytes, &msg); err != nil {
+			log.Println("JSON parse error:", err)
+			continue
 		}
 
-		val, err := redis.Get("last_message")
-		if err != nil {
-			log.Println("Redis GET error:", err)
-		}
-
-		reply := fmt.Sprintf("Echo (stored in Redis): %s", val)
-		if err := conn.WriteMessage(websocket.TextMessage, []byte(reply)); err != nil {
-			log.Println("Write error:", err)
-			break
+		if msg.Cmd == "SET" {
+			log.Printf("Updating %s -> %s\n", msg.Key, msg.Value)
+			redis.Set(msg.Key, msg.Value, 24*time.Hour) // store for a day
 		}
 	}
 }
 
 func main() {
+	// Initialize Redis connection
 	redis.InitRedis("localhost:6379", "", 0)
 
+	// Set up WebSocket handler
 	http.HandleFunc("/ws", wsHandler)
 
-	addr := ":8080"
-	fmt.Println("WebSocket server listening on", addr)
-	if err := http.ListenAndServe(addr, nil); err != nil {
-		log.Fatal("Server error:", err)
-	}
+	// Start server
+	log.Println("Server starting on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/csessh/1M-backend
 go 1.24.3
 
 require (
+	github.com/gorilla/websocket v1.5.3
+	github.com/redis/go-redis/v9 v9.12.1
+)
+
+require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/go-chi/chi/v5 v5.2.2 // indirect
-	github.com/gorilla/websocket v1.5.3 // indirect
-	github.com/redis/go-redis/v9 v9.12.1 // indirect
 )
